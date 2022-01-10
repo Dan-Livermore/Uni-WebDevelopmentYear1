809dbf5c50ecd5574290a81ec42ff7d00342e92f
Dan-Livermore <daniel.livermore@students.plymouth.ac.uk>
2022-01-10 10:23:14 +0000

    Got state changing working and furthered calendar

---
+++ wwroot/javascript.js
@@ -0,0 +1,15 @@
+function changeState(currentID) {
+    if (currentID === "Account"){
+    document.getElementById("BreakdownState").style.display="none";
+    document.getElementById("CalendarState").style.display="none";
+    document.getElementById("AccountState").style.display="block";
+    } else if (currentID === "Calendar"){
+    document.getElementById("BreakdownState").style.display="none";
+    document.getElementById("CalendarState").style.display="block";
+    document.getElementById("AccountState").style.display="none";
+    } else{
+    document.getElementById("BreakdownState").style.display="block";
+    document.getElementById("CalendarState").style.display="none";
+    document.getElementById("AccountState").style.display="none";
+    }
+}
