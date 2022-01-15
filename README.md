2adcec8461968c0ef27b909db15b0f8a969bf71e
Dan-Livermore <daniel.livermore@students.plymouth.ac.uk>
2022-01-15 14:37:36 +0000

    Improving and readme updated

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # comp1004---main-assignment-Dan-Livermore
-comp1004---main-assignment-Dan-Livermore created by GitHub Classroom
-03/10/21 - I have created this read me that I will update as I progress through the project.
+This is the repository for my time review application protype.
 
-07/01/21 - I should have connected the correct repo to my github
+This prototype contains the first 2 sprints of the application, one of which has been completed since completing the report.
+
+In this build the user can change which state of the webpage they are on, select a day in January 2022 and enter the data on the selected day.
+After this they can change states to the Breakdown section and the data they have entered should be represented there.
+
+There account window has entry boxes for a username and password, these are not currently used but it is used to show how to swap between the states of the webpage.
+
+Following this, I intend to store the users data into JSON files so more than one set of data can be stored over time, 
+so eventually the program can compare weeks to see when the user was more active.
+Then I will improve the breakdown window to show a more colourful and exciting representation of this data using the Amazon Alexa to present this data to the user.
+
+
+I used this tutorial to understand how to change, which state is currently active: https://javascript.plainenglish.io/create-a-single-page-website-using-node-js-and-express-js-a0b53e396e4f
+As well as W3Schools.com for general help for the javascript.
--- wwroot/javascript.js
+++ wwroot/javascript.js
@@ -1,7 +1,43 @@
+var a = "temp";
+var b = "temp";
+var c = "temp";
+var d = "temp";
+
 function hideStates(){
     document.getElementById("BreakdownState").style.display="none";
     document.getElementById("CalendarState").style.display="none";
     document.getElementById("DataState").style.display="none";
+}
+
+
+function loadDay(title){
+    document.getElementById("DataState").style.display="block";
+    var x = document.getElementById("title");
+    x.innerHTML = title;
+}
+
+
+function storeData(title, aname, tspent, tused){
+    var w  = "Day:              " + title;
+    a = a.replace("temp", w);
+
+    var x = document.getElementById("BreakdownAppName").innerHTML;
+    x = x.toString().replace(x,"App Name:         " + aname);
+
+    var y = document.getElementById("BreakdownTimeSpent").innerHTML;
+    y = y.toString().replace(y,"Total Time Spent: " + tspent);
+
+    var z = document.getElementById("BreakdownTimesUsed").innerHTML;
+    z = z.toString().replace(z,"Times Used:       " + tused);
+    
+}
+
+
+function fillData(w,x,y,z){
+    document.getElementById("BreakdownDate").innerHTML = w;
+    document.getElementById("BreakdownAppName").innerHTML = x;
+    document.getElementById("BreakdownTimeSpent").innerHTML = y;
+    document.getElementById("BreakdownTimesUsed").innerHTML = z;
 }
 
 
@@ -21,36 +57,6 @@ function changeState(currentID) {
     document.getElementById("CalendarState").style.display="none";
     document.getElementById("AccountState").style.display="none";
     document.getElementById("DataState").style.display="none";
+    fillData(document.getElementById('title').innerHTML, document.getElementById('aname').value, document.getElementById('tspent').value, document.getElementById('tused').value);
     }
-    
-}
-
-function loadDay(Title){
-    document.getElementById("DataState").style.display="block";
-    var x = document.getElementById("Title");
-    x.innerHTML = Title;
-}
-
-
-function storeData(title, aname, tspent, tused){
-    var w = document.getElementById("BreakdownDate").innerHTML;
-    console.log(w);
-    w = w.toString().replace(w,"Day:  " + title.innerHTML);
-    console.log(w);
-    document.getElementById("BreakdownDate").innerHTML = w;
-
-    var x = document.getElementById("BreakdownAppName").innerHTML;
-    console.log(x);
-    x = x.toString().replace(x,"App Name:  " + aname.innerHTML);
-    console.log(x);
-    document.getElementById("BreakdownAppName").innerHTML = x;
-
-    var y = document.getElementById("BreakdownTimeSpent").innerHTML;
-    y = y.toString().replace(y,"Total Time Spent:  " + tspent.innerHTML);
-    document.getElementById("BreakdownTimeSpent").innerHTML = y;
-
-    var z = document.getElementById("BreakdownTimesUsed").innerHTML;
-    z = z.toString().replace(z,"Times Used:  " + tused.innerHTML);
-    document.getElementById("BreakdownTimesUsed").innerHTML = z;
-
 }
